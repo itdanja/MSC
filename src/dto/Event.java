26c6ce25e37270348ec846a7f111d45014e6e9c8
Simon-JunhoLee <leejunho0722@naver.com>
2021-04-15 17:49:20 +0900

    Add files via upload

---
+++ src/dto/Event.java
@@ -0,0 +1,49 @@
+package dto;
+
+public class Event {
+	private String eventID;
+	private String creatorE;
+	private String titleE;
+	private String descriptionE;
+	private String categoryE;
+	private String updateTime;
+	
+	
+	public String getEventID() {
+		return eventID;
+	}
+	public void setEventID(String eventID) {
+		this.eventID = eventID;
+	}
+	public String getCreatorE() {
+		return creatorE;
+	}
+	public void setCreatorE(String creatorE) {
+		this.creatorE = creatorE;
+	}
+	public String getTitleE() {
+		return titleE;
+	}
+	public void setTitleE(String titleE) {
+		this.titleE = titleE;
+	}
+	public String getDescriptionE() {
+		return descriptionE;
+	}
+	public void setDescriptionE(String descriptionE) {
+		this.descriptionE = descriptionE;
+	}
+	public String getCategoryE() {
+		return categoryE;
+	}
+	public void setCategoryE(String categoryE) {
+		this.categoryE = categoryE;
+	}
+	public String getUpdateTime() {
+		return updateTime;
+	}
+	public void setUpdateTime(String updateTime) {
+		this.updateTime = updateTime;
+	}
+	
+}
